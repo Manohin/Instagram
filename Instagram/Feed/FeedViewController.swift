fd05866c6cfda36c7b977638aedacc009f25a56e
Manohin <111537419+Manohin@users.noreply.github.com>
2023-06-26 21:21:52 +0300

    Prepare

---
+++ Instagram/Feed/FeedViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FeedViewController.swift
+//  Instagram
+//
+//  Created by Alexey Manokhin on 26.06.2023.
+//
+
+import SnapKit
+import UIKit
+
+class FeedViewController: UIViewController {
+    
+    //MARK: - View lifecycle
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .yellow
+    }
+    
+}
